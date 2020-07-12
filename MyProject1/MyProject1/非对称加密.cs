a4aebd531033bd56870b85442f5eef3c98a733b1
architect-ghpython <47637542+architect-ghpython@users.noreply.github.com>
2020-07-13 04:10:46 +0800

    Add files via upload

---
+++ MyProject1/MyProject1/非对称加密.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject1
+{
+
+    /// <summary>
+    ///
+    /// </summary>
+    public class RSADecryptEncrypt
+    {
+
+
+        private static readonly Encoding Encoder = Encoding.UTF8;
+        /// <summary>
+        /// 公钥加密
+        /// </summary>
+        /// <param name="xmlPublicKey">公钥</param>
+        /// <param name="EncryptString">加密字符串</param>
+        /// <returns></returns>
+        public static String Encrypt(string xmlPublicKey, string EncryptString)
+        {
+            using (RSACryptoServiceProvider RSACryptography = new RSACryptoServiceProvider())
+            {
+
+                RSACryptography.FromXmlString(xmlPublicKey);
+                Byte[] PlaintextData = RSADecryptEncrypt.Encoder.GetBytes(EncryptString);
+                int MaxBlockSize = RSACryptography.KeySize / 8 - 11;//加密块最大长度限制
+
+                if (PlaintextData.Length <= MaxBlockSize)
+                    return Convert.ToBase64String(RSACryptography.Encrypt(PlaintextData, false));
+
+                using (MemoryStream PlaiStream = new MemoryStream(PlaintextData))
+                using (MemoryStream CrypStream = new MemoryStream())
+                {
+                    Byte[] Buffer = new Byte[MaxBlockSize];
+                    int BlockSize = PlaiStream.Read(Buffer, 0, MaxBlockSize);
+
+                    while (BlockSize > 0)
+                    {
+                        Byte[] ToEncrypt = new Byte[BlockSize];
+                        Array.Copy(Buffer, 0, ToEncrypt, 0, BlockSize);
+
+                        Byte[] Cryptograph = RSACryptography.Encrypt(ToEncrypt, false);
+                        CrypStream.Write(Cryptograph, 0, Cryptograph.Length);
+
+                        BlockSize = PlaiStream.Read(Buffer, 0, MaxBlockSize);
+                    }
+
+                    return Convert.ToBase64String(CrypStream.ToArray(), Base64FormattingOptions.None);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 解密
+        /// </summary>
+        /// <param name="xmlPublicKey"></param>
+        /// <param name="EncryptString"></param>
+        /// <returns></returns>
+        public static String Decrypt(string xmlPrivateKey, string EncryptString)
+        {
+            using (RSACryptoServiceProvider RSACryptography = new RSACryptoServiceProvider())
+            {
+                RSACryptography.FromXmlString(xmlPrivateKey);
+                Byte[] CiphertextData = Convert.FromBase64String(EncryptString);
+                int MaxBlockSize = RSACryptography.KeySize / 8;    //解密块最大长度限制
+
+                if (CiphertextData.Length <= MaxBlockSize)
+                    return RSADecryptEncrypt.Encoder.GetString(RSACryptography.Decrypt(CiphertextData, false));
+
+                using (MemoryStream CrypStream = new MemoryStream(CiphertextData))
+                using (MemoryStream PlaiStream = new MemoryStream())
+                {
+                    Byte[] Buffer = new Byte[MaxBlockSize];
+                    int BlockSize = CrypStream.Read(Buffer, 0, MaxBlockSize);
+
+                    while (BlockSize > 0)
+                    {
+                        Byte[] ToDecrypt = new Byte[BlockSize];
+                        Array.Copy(Buffer, 0, ToDecrypt, 0, BlockSize);
+
+                        Byte[] Plaintext = RSACryptography.Decrypt(ToDecrypt, false);
+                        PlaiStream.Write(Plaintext, 0, Plaintext.Length);
+
+                        BlockSize = CrypStream.Read(Buffer, 0, MaxBlockSize);
+                    }
+
+                    return RSADecryptEncrypt.Encoder.GetString(PlaiStream.ToArray());
+                }
+            }
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    class 非对称加密
+    {
+    }
+}
